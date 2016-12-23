1e4f1ca7ddaef17e7c33c4e3a47ed9983c68c6c2
vasiliev-alexey <alex_v81@mail.ru>
2016-12-23 20:51:52 +0300

    Разработка диалогов в java FX +  борьба с потерей фокуса

---
+++ uifx/src/main/java/com/av/ui/controllers/dilalogs/ModelFormController.java
@@ -0,0 +1,134 @@
+package com.av.ui.controllers.dilalogs;
+
+import com.av.domain.GroupType;
+import com.av.domain.Model;
+import com.av.domain.ModelAttrGroup;
+import com.av.ui.controllers.AbstractController;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.stage.Stage;
+import org.springframework.beans.BeanUtils;
+import org.springframework.stereotype.Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by vasiliev-alexey on 23.12.16.
+ */
+public class ModelFormController extends AbstractController   {
+
+    private Model model;
+    private Model savedModel;
+    private boolean okClicked   = false;
+    private ObservableList<ModelAttrGroup> groups = FXCollections.observableArrayList();
+
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+
+    private Stage dialogStage;
+    @FXML
+    private TextField txtCode;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPackage;
+
+    @FXML
+    private TableView<ModelAttrGroup> tableGroup;
+    @FXML
+    private TableColumn<ModelAttrGroup , String> codeColumn;
+    @FXML
+    private TableColumn<ModelAttrGroup , String> nameColumn;
+
+    @FXML
+    private  TableColumn<ModelAttrGroup, String> typeColumn;
+
+    @FXML
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public boolean isOkClicked() {
+
+        return okClicked;
+    }
+
+
+
+
+    public void setModel(Model model) {
+        groups.clear();
+        this.model = model;
+
+        groups = FXCollections.observableArrayList(model.getModelAttrGroups());
+        nameColumn.setCellValueFactory(new PropertyValueFactory<ModelAttrGroup, String>("name"));
+        codeColumn.setCellValueFactory(new PropertyValueFactory<ModelAttrGroup, String>("code"));
+
+        codeColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+
+        codeColumn.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<ModelAttrGroup, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<ModelAttrGroup, String> event) {
+
+                event.getTableView().getItems().get(event.getTablePosition().getRow()).setCode(event.getNewValue());
+
+
+            }
+        });
+
+        typeColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getGroupType().toString()));
+        typeColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        typeColumn.setOnEditCommit(new EventHandler<TableColumn.CellEditEvent<ModelAttrGroup, String>>() {
+            @Override
+            public void handle(TableColumn.CellEditEvent<ModelAttrGroup, String> event) {
+                event.getRowValue().setGroupType(GroupType.valueOf(event.getNewValue()));
+
+
+            }
+        });
+        txtCode.textProperty().bindBidirectional(this.model.codeProperty());
+        txtName.textProperty().bindBidirectional(this.model.modelNameProperty());
+        txtPackage.textProperty().bindBidirectional(this.model.packageNameProperty());
+
+        tableGroup.setItems(groups);
+        tableGroup.setFixedCellSize(25);
+        tableGroup.prefHeightProperty().bind(Bindings.size(tableGroup.getItems()).multiply(tableGroup.getFixedCellSize()).add(30));
+        savedModel = new Model();
+        BeanUtils.copyProperties(model , savedModel);
+
+    }
+
+    public Model getModel() {
+        return model;
+    }
+
+
+    public void handleOk(ActionEvent actionEvent) {
+
+        okClicked = true;
+
+        dialogStage.close();
+    }
+
+    @FXML
+    private void handleCancel() {
+        BeanUtils.copyProperties(savedModel ,model );
+        savedModel = null;
+        dialogStage.close();
+    }
+}
--- uifx/src/main/java/com/av/ui/managers/ModelManager.java
+++ uifx/src/main/java/com/av/ui/managers/ModelManager.java
@@ -2,11 +2,26 @@ package com.av.ui.managers;
 
 import com.av.domain.Model;
 import com.av.repositories.ModelService;
+import com.av.ui.controllers.Controller;
+import com.av.ui.controllers.dilalogs.ModelFormController;
+import com.av.ui.utils.SpringFXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.TableSelectionModel;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
+
 import org.springframework.stereotype.Service;
 
+import javax.xml.bind.attachment.AttachmentMarshaller;
+import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,7 +30,7 @@ import java.util.logging.Logger;
  */
 @Service
 @Lazy
-public class ModelManager extends AbstractDataManager<Model>{
+public class ModelManager extends AbstractDataManager<Model> {
 
     private static Logger modelManagerLogger = Logger.getLogger(ModelManager.class.getName());
     @Autowired
@@ -24,23 +39,49 @@ public class ModelManager extends AbstractDataManager<Model>{
     public void setTableselectionModel(TableSelectionModel tableselectionModel) {
         this.tableSelectionModel = tableselectionModel;
     }
+
     @Override
     public Model editItem(Model item) {
-        modelService.Save(item);
-        modelManagerLogger.log(Level.INFO , "Edit:" + item);
+        modelManagerLogger.log(Level.INFO, "Edit:" + item);
+
+        ModelFormController controller = (ModelFormController) SpringFXMLLoader.load("/fxml/dialogs/ModelForm.fxml");
+        AnchorPane view = (AnchorPane) controller.getView();
+
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("Edit Model");
+        dialogStage.initModality(Modality.APPLICATION_MODAL);
+
+        Scene scene = new Scene(view);
+        dialogStage.setScene(scene);
+        controller.setModel(item);
+        controller.setDialogStage(dialogStage);
+        dialogStage.showAndWait();
+
+        if (controller.isOkClicked()) {
+            modelManagerLogger.info("model save");
+            modelService.Save(item);
+
+            modelManagerLogger.info("model saved");
+        }
+
+
+        //
+
+
         return item;
     }
 
     @Override
-    public Model addItem( ) {
-        modelManagerLogger.log(Level.INFO , "add item");
+    public Model addItem() {
+        modelManagerLogger.log(Level.INFO, "add item");
         return new Model();
     }
 
     @Override
     public String getLabel() {
-        return  "Управление моделями";
+        return "Управление моделями";
     }
+
     @Override
     public String getViewName() {
         return "/fxml/ModelTableView.fxml";
