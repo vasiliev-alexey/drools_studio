cdaca7595c99dfc7986678bbc048fd67635a5b44
vasiliev-alexey <alex_v81@mail.ru>
2016-12-26 17:15:38 +0300

    Юнит тесты

--- validators/src/main/java/com/av/validators/ModelValidator.java
+++ validators/src/main/java/com/av/validators/ModelValidator.java
@@ -1,14 +1,21 @@
-package com.av;
+package com.av.validators;
 
 
 import com.av.domain.Model;
 import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
 /**
  * Created by vasiliev-alexey on 26.12.16.
  */
 public class ModelValidator implements Validator {
+
+    public static final  String CODEEMPTY = "model.code.not_empty";
+    public static final  String PACKAGEEMPTY = "model.package.not_empty";
+
+
+
     @Override
     public boolean supports(Class<?> aClass) {
         return Model.class.equals(aClass);
@@ -24,6 +31,11 @@ public class ModelValidator implements Validator {
             String msg2 = "Имя модели не может быть пустым";
             errors.rejectValue("name","model.name.empty",msg2);
         }
+        ValidationUtils.rejectIfEmpty(errors, "code", CODEEMPTY,
+                "Код модели не должен быть пустым");
+
+        ValidationUtils.rejectIfEmpty(errors, "packageName", CODEEMPTY,
+                "Пакет для  модели не должен быть пустым");
 
     }
 }
---
+++ validators/src/test/java/TestModelValidator.java
@@ -0,0 +1,67 @@
+import com.av.validators.ModelValidator;
+import org.junit.Assert;
+
+import com.av.domain.Model;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.validation.MapBindingResult;
+import org.springframework.validation.ObjectError;
+
+import java.util.*;
+
+/**
+ * Created by vasiliev-alexey on 26.12.16.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ContextConfiguration(locations = {"classpath:validator-beans.xml"})
+public class TestModelValidator {
+
+
+    @Autowired
+    @Qualifier("modelValidator")
+    private ModelValidator modelValidator;
+
+    @Test
+    public void modelNameEmptyShouldThrowError() {
+        Model m = new Model();
+        m.setCode("Tst1");
+        Map<String,String> map = new HashMap<String,String>();
+        MapBindingResult err = new MapBindingResult(map, Model.class.getName());
+        modelValidator.validate(m , err);
+        Assert.assertTrue("Ошибка имя модели не заполнено" , err.hasErrors() );
+    }
+
+
+    @Test
+    public void modelCodeEmptyShouldThrowError() {
+        Model m = new Model();
+        m.setModelName("Tst1");
+        Map<String,String> map = new HashMap<String,String>();
+        MapBindingResult err = new MapBindingResult(map, Model.class.getName());
+        modelValidator.validate(m , err);
+        List<ObjectError> list = err.getAllErrors();
+        Optional<ObjectError> codeErr = list.stream().filter(e ->  ModelValidator.CODEEMPTY.equals(e.getCode())).findFirst();
+        Assert.assertTrue("Ошибка Код модели не заполнено" , codeErr.isPresent());
+    }
+
+    public void modelPackageEmptyShouldThrowError() {
+        Model m = new Model();
+        m.setModelName("Tst1");
+        Map<String,String> map = new HashMap<String,String>();
+        MapBindingResult err = new MapBindingResult(map, Model.class.getName());
+        modelValidator.validate(m , err);
+        List<ObjectError> list = err.getAllErrors();
+        Optional<ObjectError> codeErr = list.stream().filter(e ->  ModelValidator.CODEEMPTY.equals(e.getCode())).findFirst();
+        Assert.assertTrue("Ошибка Код модели не заполнено" , codeErr.isPresent());
+    }
+
+
+
+}
