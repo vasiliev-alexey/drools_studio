329636c53c6c25cd23ce0bb2910919b5efb696d6
vasiliev-alexey <alex_v81@mail.ru>
2016-12-29 15:07:45 +0300

    caffein  cache config

--- repository/src/main/java/com/av/repositories/ModelService.java
+++ repository/src/main/java/com/av/repositories/ModelService.java
@@ -2,12 +2,15 @@ package com.av.repositories;
 
 import com.av.domain.Model;
 import javafx.collections.ObservableList;
+import org.springframework.cache.annotation.CacheEvict;
+
 
 /**
  * Created by alexey on 25.11.16.
  */
 public interface ModelService {
 
+    @CacheEvict
     Model Save(Model model);
 
 
