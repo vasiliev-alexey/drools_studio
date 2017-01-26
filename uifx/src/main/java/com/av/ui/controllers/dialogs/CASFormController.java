64ac5bab660b224a34e80f4d0e3f671d7ce5f0cc
vasiliev-alexey <alex_v81@mail.ru>
2017-01-26 09:19:39 +0300

    Форма редактирования  для структуры плана счетов

---
+++ uifx/src/main/java/com/av/ui/controllers/dialogs/CASFormController.java
@@ -0,0 +1,21 @@
+package com.av.ui.controllers.dialogs;
+
+import com.av.ui.controllers.AbstractController;
+import com.av.ui.controllers.Controller;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+
+/**
+ * Created by vasiliev-alexey on 26.01.17.
+ */
+public class CASFormController extends AbstractController {
+
+
+
+
+    public void handleOk(ActionEvent actionEvent) {
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+}
